b669b6a4a472b3dd65ab93e3add5407a79ff1f93
Carla Bosco <carlabosco@gmail.com>
2019-05-01 15:05:18 -0700

    Added show products

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 describe CartsController do
-  # describe "show" do
-  #   it "shows the cart even if empty" do
-  #     @order_products.count = 0
+  describe "show" do
+    # it "shows the cart even if empty" do
+    #   Product.destroy_all
 
-  #     expect(get cart_path).must_respond_with :success
-  #   end
-  # end
+    #   expect(get cart_path).must_respond_with :success
+    # end
+  end
 end
