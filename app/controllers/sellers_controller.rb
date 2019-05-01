bd3a634001ba03341aab9cf76b3611bcefab2fb1
sjscotton <sj.scotton@gmail.com>
2019-05-01 14:22:41 -0700

    added seller show action

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,2 +1,9 @@
 class SellersController < ApplicationController
+  def show
+    @seller = Seller.find_by(id: params[:id])
+
+    unless @seller
+      head :not_found
+    end
+  end
 end
--- test/controllers/sellers_controller_test.rb
+++ test/controllers/sellers_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
-describe SellersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+describe "SellersController" do
+  let (:seller) { sellers.first }
+  it "Can get a product with a valid id" do
+    get seller_path(seller.id)
+
+    must_respond_with :success
+  end
+
+  it "Will redirect if given an invalid product ID" do
+    get seller_path(-1)
+
+    must_respond_with :not_found
+  end
 end
