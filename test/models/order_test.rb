bc90e12a31a6ac3d9824ca14bff1aeb362f5fd3b
Bitaman <b.amani.s@gmail.com>
2019-05-01 15:08:58 -0700

    added the total_price column to orders

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -6,22 +6,28 @@ describe Order do
   # it "must be valid" do
   #   value(order).must_be :valid?
   # end
-  describe "validations" do
-    it "has a list of products" do
+  describe "relations" do
+    it "has a list of order_products" do
       order = orders(:one)
       order.must_respond_to :OrderProducts
-      order.products.each do |product|
+      order.orderproducts.each do |product|
         product.must_be_kind_of OrderProduct
       end
     end
-  end
-
-  describe "relations" do
     it "can add an order_product through .order_products" do
       order_prod = OrderProduct.new(quantity: 1)
       order_one = orders(:one)
       order_one.order_products << order_prod
       expect(order_one.order_products).must_include order_prod
+    end
+  end
+
+  describe "validations" do
+    it "update the status befor saving the order" do
+      order = orders(:one)
+      order.status.must_equal nil
+      order.save
+      expect(order.status).must_equal "In progress"
     end
   end
 end
