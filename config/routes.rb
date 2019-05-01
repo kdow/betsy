93bdbe3c01d7f69ff7ce35c90f7f3ee22cdd9b89
sjscotton <sj.scotton@gmail.com>
2019-05-01 16:27:44 -0700

    merging"

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,10 @@ Rails.application.routes.draw do
   resources :order_products
   resource :cart, only: [:show]
 
-<<<<<<< HEAD
   resources :products, only: [:index, :show, :new, :create, :edit]
   resources :sellers, only: [:show]
-=======
-  resources :products, only: [:index, :show, :new, :create]
 
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "sellers#create", as: "auth_callback"
   delete "/logout", to: "sellers#destroy", as: "logout"
->>>>>>> 04cebd6ce873052b07f554a1e48acc17c8e03440
 end
