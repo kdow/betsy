93941a7985ad63743d6cbd5e8f54e7a8f54a9bd1
Kelly Downes <kedownes@gmail.com>
2019-05-01 14:44:38 -0700

    Add seller create action

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,2 +1,22 @@
 class SellersController < ApplicationController
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    seller = Seller.find_by(uid: auth_hash[:uid], provider: "github")
+    if seller
+      flash[:success] = "Logged in as returning seller #{seller.name}"
+    else
+      seller = seller.build_from_github(auth_hash)
+
+      if seller.save
+        flash[:success] = "Logged in as new seller #{user.name}"
+      else
+        flash[:error] = "Could not create new seller account: #{seller.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    session[:seller_id] = seller.id
+    return redirect_to root_path
+  end
 end
