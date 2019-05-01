28ca7cee744c8e0b3c9eeaaed9704f687039b33a
Kelly Downes <kedownes@gmail.com>
2019-05-01 10:30:09 -0700

    Add relationships to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_233212) do
+ActiveRecord::Schema.define(version: 2019_04_30_235458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 2019_04_30_233212) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -66,5 +70,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_233212) do
     t.string "email"
   end
 
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "sellers"
 end
