e88776a5ab376275a74cd786f7c45629fd8cfdfa
sjscotton <sj.scotton@gmail.com>
2019-05-01 11:08:31 -0700

    added product create and tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    # @order_item = current_order.order_products.new
   end
 
   def show
@@ -14,5 +15,26 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new product_params
+    @successful = @product.save
+    if @successful
+      redirect_to product_path(@product.id)
+      # flash[:status] = :success
+      # flash[:message] = "successfully saved a product with ID #{@product.id}"
+    else
+      # flash.now[:status] = :warning
+      # flash.now[:message] = "A problem occurred: Could not create #{@product.category}"
+      # @errors = @product.errors
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def product_params
+    return params.require(:product).permit(:name, :price, :quantity, :seller_id, :description)
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,9 +1,13 @@
 require "test_helper"
 
 describe ProductsController do
-  let (:product) {
-    Product.create name: "kittin mittins", description: "paw warmers", price: 1200
+  let (:seller) {
+    Seller.create username: "kittin mittin seller"
   }
+  let (:product) {
+    Product.create name: "kittin mittins", description: "paw warmers", price: 1200, seller_id: seller.id
+  }
+
   describe "index" do
     it "can get the index page" do
       get products_path
@@ -31,5 +35,47 @@ describe ProductsController do
 
       must_respond_with :success
     end
+  end
+
+  describe "create" do
+    it "can create a new product" do
+      product_hash = {
+        product: {
+          name: "Kitty tower",
+          price: 1300,
+          description: "Kitty fun tower",
+          quantity: 5,
+          seller_id: seller.id,
+        },
+      }
+      expect {
+        post products_path, params: product_hash
+      }.must_change "Product.count", 1
+
+      # check_flash
+
+      new_product = Product.find_by(name: product_hash[:product][:name])
+      expect(new_product.name).must_equal product_hash[:product][:name]
+
+      must_respond_with :redirect
+      must_redirect_to product_path(new_product.id)
+    end
+
+    # it "sends a bad_request status if given bad data" do
+    #   product_hash = {
+    #     product: {
+    #       name: " ",
+    #     },
+    #   }
+    #   expect(Product.new(product_hash[:product])).wont_be :valid?
+
+    #   expect {
+    #     post products_path, params: product_hash
+    #   }.wont_change "product.count"
+
+    #   must_respond_with :bad_request
+
+    #   check_flash(:warning)
+    # end
   end
 end
