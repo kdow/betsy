b53459cead8c7c06952c39fa20017e26c54cc94c
Kelly Downes <kedownes@gmail.com>
2019-05-01 14:09:05 -0700

    Add relations test for Order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -6,4 +6,13 @@ describe Order do
   it "must be valid" do
     value(order).must_be :valid?
   end
+
+  describe "relations" do
+    it "can add an order_product through .order_products" do
+      order_prod = OrderProduct.new(quantity: 1)
+      order_one = order(:one)
+      order_one.order_products << order_prod
+      expect(product_one.order_products).must_include order_prod
+    end
+  end
 end
