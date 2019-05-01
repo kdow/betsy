a28daa35c094acf43000ed05621e85944bb22a30
sjscotton <sj.scotton@gmail.com>
2019-04-30 20:56:40 -0700

    added product#show and tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    unless @product
+      head :not_found
+      return
+    end
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,11 +1,28 @@
 require "test_helper"
 
 describe ProductsController do
+  let (:product) {
+    Product.create name: "kittin mittins", description: "paw warmers", price: 1200
+  }
   describe "index" do
     it "can get the index page" do
       get products_path
 
       must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "Can get a product with a valid id" do
+      get products_path(product.id)
+
+      must_respond_with :success
+    end
+
+    it "Will redirect if given an invalid product ID" do
+      get product_path(-1)
+
+      must_respond_with :not_found
     end
   end
 end
