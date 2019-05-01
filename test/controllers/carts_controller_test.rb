f1f76082960752fc2c9fac63210182b7b6e6bd86
Carla Bosco <carlabosco@gmail.com>
2019-05-01 14:11:58 -0700

    Added show test to cart

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe CartsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  # describe "show" do
+  #   it "shows the cart even if empty" do
+  #     @order_products.count = 0
+
+  #     expect(get cart_path).must_respond_with :success
+  #   end
   # end
 end
