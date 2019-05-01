08936090ee6b38ab6da27065d438605a4c203c34
Kelly Downes <kedownes@gmail.com>
2019-05-01 14:13:42 -0700

    Add seller relations test and fix order relations

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -10,9 +10,9 @@ describe Order do
   describe "relations" do
     it "can add an order_product through .order_products" do
       order_prod = OrderProduct.new(quantity: 1)
-      order_one = order(:one)
+      order_one = orders(:one)
       order_one.order_products << order_prod
-      expect(product_one.order_products).must_include order_prod
+      expect(order_one.order_products).must_include order_prod
     end
   end
 end
