438f1a146d0ca742d699eead7ecfbe8417c89bf0
Bitaman <b.amani.s@gmail.com>
2019-05-01 15:56:08 -0700

    pushing my changes to cat-cart

--- test/controllers/order_products_controller_test.rb
+++ test/controllers/order_products_controller_test.rb
@@ -1,14 +1,18 @@
 require "test_helper"
 
-# describe OrderProductsController do
-# let(:existing_order_product) {order_products(:one)}
-# it "create an order product with valid data for a real product" do
-#   new_order_product = {order_product: {quantity: 3, product_id: 2}}
-#   expect {
-#     post 
-#   }
+describe OrderProductsController do
+let(:existing_order_product) {order_products(:one)}
+it "create an order product with valid data for a real product" do
+  new_order_product = {order_product: {quantity: 3, product_id: 2}}
+  expect {
+    post order_products_path, params: new_order_products
+}.must_change "OrderProduct.count", 1
+
+new_order_product_id = OrderProduct.find_by(product_id: 2).id
+
+must_respond_with :redirect
+must_redirect_to work_path(new_work_id)
 
 
 
-
-# end
+end
