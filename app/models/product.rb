3c94cd30b663a5709b0569d685e4eeb609072b5b
Kelly Downes <kedownes@gmail.com>
2019-05-01 10:42:24 -0700

    Add validations to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :seller
   has_many :order_products
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates_numericality_of :price,
+    only_integer: true,
+    greater_than: 0
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,3 +1,6 @@
 class Seller < ApplicationRecord
   has_many :products
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
