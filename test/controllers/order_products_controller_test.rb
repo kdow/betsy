a0e9c5aa29c54d823689be88e3f3e8dca4970692
Bitaman <b.amani.s@gmail.com>
2019-05-02 09:31:01 -0700

    getting the master changes

--- test/controllers/order_products_controller_test.rb
+++ test/controllers/order_products_controller_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 describe OrderProductsController do
+  let
   describe "create" do
-    it "can create order products " do
+    it "can create an order product " do
     end
   end
 end
