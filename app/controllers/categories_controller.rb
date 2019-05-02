12a63c34460003382b45cbdf4cdda421d5381491
Kelly Downes <kedownes@gmail.com>
2019-05-02 16:07:13 -0700

    Add show action to Categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,12 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+
+    unless @category
+      head :not_found
+    end
+  end
 end
