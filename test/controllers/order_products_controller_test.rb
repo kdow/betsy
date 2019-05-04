e9a1938eacb6481e8c3018edd4ae35ffc537cd89
Carla Bosco <carlabosco@gmail.com>
2019-05-04 10:25:11 -0700

    Added tests for order_product

--- test/controllers/order_products_controller_test.rb
+++ test/controllers/order_products_controller_test.rb
@@ -1,6 +1,22 @@
 require "test_helper"
 
 describe OrderProductsController do
+  describe "create" do
+    it "should get created" do
+
+      proc {
+        post order_products_path,
+        params: {
+          order_product: {
+            quantity: 4,
+            product_id: Product.first.id,
+          }
+        }
+      }.must_change "OrderProduct.count", 1
+      must_respond_with :redirect
+      must_redirect_to products_path
+    end
+  end
 
   it "refuses order if quantity greater than inventory" do
 
@@ -22,5 +38,20 @@ describe OrderProductsController do
 
     expect(test_item.quantity).must_equal before_inventory
     
+  end
+
+  it "updates quantity in the cart" do
+    skip
+    proc {
+      patch order_product_path(order_products(:one).quantity),
+      params: {
+        order_product: {
+          quantity: 4,
+          product_id: Product.first.id,
+        }
+      }
+    }.must_change "OrderProduct.quantity", +1
+    must_respond_with :redirect
+    must_redirect_to order_product_path
   end
 end
