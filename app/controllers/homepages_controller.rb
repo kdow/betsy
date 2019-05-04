a4f31bbfcc6c5d8832ad71b169ec1d89531cd5c2
Kelly Downes <kedownes@gmail.com>
2019-05-03 21:52:32 -0700

    Add homepage route, index action and content

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,8 @@
 class HomepagesController < ApplicationController
+  skip_before_action :require_login
+  skip_before_action :auth_seller
+
   def index
-    @products = Products.all
+    @products = Product.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "products#index"
+  root "homepages#index"
 
   resources :order_products
   resource :cart, only: [:show]
