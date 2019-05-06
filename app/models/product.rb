9989709210adc958d3ccbc566cdf34fb770f155d
Bitaman <b.amani.s@gmail.com>
2019-05-05 20:26:52 -0700

    added more validations to order

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,14 @@ class Product < ApplicationRecord
   belongs_to :seller
   has_many :order_products
   has_and_belongs_to_many :categories
+  has_many :orders, through: :order_products
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
   validates_numericality_of :price,
     only_integer: true,
     greater_than: 0
+  validates :description, presence: true
 
   def self.adjust_quantity(order_products)
     order_products.each do |item|
