3541b42dbc3ec5b0c422a6f4732f92b0f9bb9fa9
Bitaman <b.amani.s@gmail.com>
2019-05-08 10:38:54 -0700

    added the self method to orderproduct to check if the item is already in the cart

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -6,9 +6,18 @@ class OrderProduct < ApplicationRecord
   validates_numericality_of :quantity,
                             only_integer: true,
                             greater_than: 0
-                          
 
   def line_item_subtotal
     return self.quantity * self.product.price
+  end
+
+  def self.already_in_cart?(order_product, order)
+    result = self.all.where(product_id: order_product.product_id, order_id: order.id)
+
+    if result.empty?
+      return false
+    else
+      return true
+    end
   end
 end
